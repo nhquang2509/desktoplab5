82bb4027fb32e5122e9a73d347bbfebdbbe9242e
nhquang2509 <164120511+nhquang2509@users.noreply.github.com>
2024-09-24 10:50:14 +0700

    Add files via upload

---
+++ lab5/lab5/Form1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+using System.IO;
+using Newtonsoft.Json.Linq;
+
+namespace lab5
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public class StudentInfo
+        {
+            // Các thuộc tính
+            public string MSSV { get; set; }
+            public string Hoten { get; set; }
+            public int Tuoi { get; set; }
+            public double Diem { get; set; }
+            public bool TonGiao { get; set; }
+            // Phương thức tạo lập
+            public StudentInfo(string mssv, string hoten, int tuoi, double
+           diem, bool tongiao)
+            {
+                this.MSSV = mssv;
+                this.Hoten = hoten;
+                this.Tuoi = tuoi;
+                this.Diem = diem;
+                this.TonGiao = tongiao;
+            }
+        }
+
+        /// <summary>
+        /// Phương thức đọc tập tin JSON
+        /// </summary>
+        /// <param name="Path">Đường dẫn tập tin</param>
+        /// <returns>Danh sách các đối tượng từ tập tin JSON</returns>
+        private List<StudentInfo> LoadJSON(string Path)
+        {
+            // Khai báo danh sách lưu trữ
+            List<StudentInfo> List = new List<StudentInfo>();
+            // Đối tượng đọc tập tin
+            StreamReader r = new StreamReader(Path);
+            string json = r.ReadToEnd(); // Đọc hết
+                                         // Chuyển về thành mảng các đối tượng
+            var array = (JObject)JsonConvert.DeserializeObject(json);
+            // Lấy đối tượng sinhvien
+            var students = array["sinhvien"].Children();
+            foreach (var item in students) // Duyệt mảng
+            {
+                // Lấy các thành phần
+                string mssv = item["MSSV"].Value<string>();
+                string hoten = item["hoten"].Value<string>();
+                int tuoi = item["tuoi"].Value<int>();
+                double diem = item["diem"].Value<double>();
+                bool tongiao = item["tongiao"].Value<bool>();
+                // Chuyển vào đối tượng StudentInfo
+                StudentInfo info = new StudentInfo(mssv, hoten, tuoi, diem,
+               tongiao);
+                List.Add(info);// Thêm vào danh sách
+            }
+            return List;
+        }
+
+        private void btnJSon_Click(object sender, EventArgs e)
+        {
+            string Str = ""; // chuỗi lưu trữ
+            string Path = "Z:\\lab5\\lab5\\json1.json"; // Đường dẫn tập tin
+            List<StudentInfo> List = LoadJSON(Path); // Gọi phương thức
+            for (int i = 0; i < List.Count; i++) // Đọc danh sách
+            {
+                StudentInfo info = List[i];
+                Str += string.Format("Sinh viên {0} có MSSV: {1}, họ tên: {2},điểm TB: {3}\r\n", (i + 1), info.MSSV, info.Hoten, info.Diem);
+            }
+            MessageBox.Show(Str);
+        }
+    }
+}
+
